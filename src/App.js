23153de0beddf1dcd73743eb18cdf406d490db5b
Allana Karla <allanakarla1702@gmail.com>
2022-02-02 00:48:23 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
@@ -26,12 +26,38 @@ class App extends React.Component {
     const { target } = event;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const { name } = target;
-    this.setState({
-      [name]: value });
+    // segundo parâmetro só vai acontecer quando o estado estiver att;
+    this.setState(() => ({
+      [name]: value }), () => this.getCondition());
   }
 
   onSaveButtonClick() {
     return 'oii';
+  }
+
+  getCondition() {
+    const NUM_MAX = 90;
+    const SUM_NUM_MAX = 210;
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+    const arrString = [cardName, cardDescription, cardImage];
+    // some: se pelo menos um elemento for "" retorna true;
+    const boolString = arrString.some((state) => state === '');
+    const boolNumber1 = +cardAttr1 >= 0 && +cardAttr1 <= NUM_MAX;
+    const boolNumber2 = +cardAttr2 >= 0 && +cardAttr2 <= NUM_MAX;
+    const boolNumber3 = +cardAttr3 >= 0 && +cardAttr3 <= NUM_MAX;
+    // super dica do Bruno Alves: o + na frente da variável transforma em number
+    const boolSum = (+cardAttr1 + +cardAttr2 + +cardAttr3) <= SUM_NUM_MAX;
+    // só retorna true se todos forem true, caso contrário retorna false
+    const result = !boolString && boolNumber1 && boolNumber2 && boolNumber3 && boolSum;
+    this.setState({
+      // caso seja disabled, está desabilitado;
+      isSaveButtonDisabled: !result,
+    });
   }
 
   render() {
@@ -44,7 +70,7 @@ class App extends React.Component {
           onSaveButtonClick={ this.onSaveButtonClick }
           { ...this.state }
         />
-        <Card onInputChange={ this.onInputChangeFunc } { ...this.state } />
+        <Card { ...this.state } />
       </div>
     );
   }
