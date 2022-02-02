0e3a2d86a1a720dae2a9f704c2e0b3c3c6ce08f0
Allana Karla <allanakarla1702@gmail.com>
2022-02-02 12:00:04 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
+      deck: [],
     };
+
     this.onInputChangeFunc = this.onInputChangeFunc.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
@@ -31,8 +33,39 @@ class App extends React.Component {
       [name]: value }), () => this.getCondition());
   }
 
-  onSaveButtonClick() {
-    return 'oii';
+  onSaveButtonClick(event) {
+    event.preventDefault();
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo } = this.state;
+      // Após o botão salvar, limpa os inputs
+    this.setState((prevState) => (
+      // não é o prevState!!
+      { cardName: '',
+        cardDescription: '',
+        cardImage: '',
+        cardAttr1: '0',
+        cardAttr2: '0',
+        cardAttr3: '0',
+        cardRare: 'normal',
+        cardTrunfo: false,
+        // Add prevState e novo estado
+        deck:
+        [...prevState.deck,
+          { cardName,
+            cardDescription,
+            cardImage,
+            cardAttr1,
+            cardAttr2,
+            cardAttr3,
+            cardRare,
+            cardTrunfo }] }
+    ));
   }
 
   getCondition() {
